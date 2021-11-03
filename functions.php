2e96b0cab6a8e0462dc895a4120976e5545a2b2f
Nemolite <g25092011@mail.ru>
2021-11-03 11:17:22 +0300

    start

---
+++ functions.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Дочерняя тема для темы newsup  
+ * 
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Helpers
+ */
+if (!function_exists('show')) {
+    function show() {
+        echo "<pre>";
+	    print_r($param);
+	    echo "</per>";
+    }
+}
+
+if (!function_exists('vshow')) {
+    function vshow() {
+        echo "<pre>";
+	    var_dump($param);
+	    echo "</per>";
+    }
+}
+
+function teh_scripts_style() {
+	$theme_version = wp_get_theme()->get( 'Version' );
+	wp_enqueue_style( 'teh-style', get_stylesheet_directory_uri() .'/assets/css/teh.css' );
+	wp_enqueue_script( 'teh-script', get_stylesheet_directory_uri() . '/assets/js/teh.js', array(), $theme_version, true );
+}
+add_action( 'wp_enqueue_scripts', 'teh_scripts_style' );
+
+/**
+ * Банеры
+ */
+require_once( get_stylesheet_directory(). '/inc/baners.php' );
+?>
